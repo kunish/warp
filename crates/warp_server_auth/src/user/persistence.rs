deeb7284640a9858fb57045597c777be4b650f4c
David Stern <david@warp.dev>
2026-05-30 22:24:55 -0400

    Create a `warp_server_auth` crate to own core auth types.

--- crates/warp_server_auth/src/user/persistence.rs
+++ crates/warp_server_auth/src/user/persistence.rs
@@ -3,10 +3,8 @@ use warp_graphql::scalars::time::ServerTimestamp;
 use warpui::AppContext;
 use warpui_extras::secure_storage::{self, AppContextExt};
 
-use crate::auth::user::{
-    AnonymousUserType, FirebaseAuthTokens, PersonalObjectLimits, UserMetadata,
-};
-use crate::auth::UserUid;
+use super::{AnonymousUserType, FirebaseAuthTokens, PersonalObjectLimits, UserMetadata};
+use crate::UserUid;
 
 const USER_STORAGE_KEY: &str = "User";
 
@@ -80,5 +78,5 @@ impl PersistedUser {
 }
 
 #[cfg(test)]
-#[path = "user_persistence_tests.rs"]
+#[path = "persistence_tests.rs"]
 mod tests;
